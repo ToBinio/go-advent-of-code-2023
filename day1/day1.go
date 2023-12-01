4d6a410ec06a92aa6f799150c130558a78341e99
to_binio <tobias.frischmann1@gmail.com>
2023-12-01 07:27:31 +0100

    day 1 part 1 & 2 done

--- day1/day1.go
+++ day1/day1.go
@@ -10,7 +10,9 @@ import (
 func Run() {
 	println("day1")
 
-	file, err := os.Open("resources/day1/example.txt")
+	numbers := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	file, err := os.Open("resources/day1/input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +26,23 @@ func Run() {
 
 		var values []string
 
+	outer:
 		for i := range line {
+
+			for index, numberText := range numbers {
+
+				if i+len(numberText) > len(line) {
+					continue
+				}
+
+				sub := line[i : i+len(numberText)]
+
+				if sub == numberText {
+					values = append(values, strconv.Itoa(index+1))
+					continue outer
+				}
+			}
+
 			char := string(line[i])
 
 			val, err := strconv.Atoi(char)
