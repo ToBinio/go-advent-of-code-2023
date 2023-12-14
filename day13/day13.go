ef479636c5afe11d9a9d64d2020a0d23a9d570a4
to_binio <tobias.frischmann1@gmail.com>
2023-12-14 07:13:53 +0100

    day 13 part 1 done

---
+++ day13/day13.go
@@ -0,0 +1,93 @@
+package day13
+
+import "advent-of-code-2023/io"
+
+func Run() {
+	lines := io.ReadLines("resources/day13/input.txt")
+
+	var notes []Note
+
+	currentNote := Note{data: [][]rune{}}
+	for _, line := range lines {
+		if line == "" {
+			notes = append(notes, currentNote)
+			currentNote = Note{data: [][]rune{}}
+			continue
+		}
+
+		var charLine []rune
+
+		for _, char := range line {
+			charLine = append(charLine, char)
+		}
+
+		currentNote.data = append(currentNote.data, charLine)
+	}
+	notes = append(notes, currentNote)
+
+	sum := 0
+
+	for _, note := range notes {
+		sum += getMirrorValue(note)
+	}
+
+	println(sum)
+}
+
+type Note struct {
+	data [][]rune
+}
+
+func getMirrorValue(note Note) int {
+	for i := 0; i < len(note.data)-1; i++ {
+		if isMirroredHorizontal(note, i) {
+			return (i + 1) * 100
+		}
+	}
+
+	for i := 0; i < len(note.data[0])-1; i++ {
+		if isMirroredVertically(note, i) {
+			return i + 1
+		}
+	}
+
+	return 0
+}
+
+func isMirroredVertically(note Note, index int) bool {
+	for i := 0; i <= index; i++ {
+		left := index - i
+		right := index + i + 1
+
+		if right >= len(note.data[0]) {
+			break
+		}
+
+		for _, line := range note.data {
+			if line[left] != line[right] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func isMirroredHorizontal(note Note, index int) bool {
+	for i := 0; i <= index; i++ {
+		left := index - i
+		right := index + i + 1
+
+		if right >= len(note.data) {
+			break
+		}
+
+		for column := 0; column < len(note.data[0]); column++ {
+			if note.data[left][column] != note.data[right][column] {
+				return false
+			}
+		}
+	}
+
+	return true
+}
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"advent-of-code-2023/day10"
+	"advent-of-code-2023/day13"
 )
 
 func main() {
-	day10.Run()
+	day13.Run()
 }
