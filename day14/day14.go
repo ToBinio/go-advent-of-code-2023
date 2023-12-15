d24305ad23e25a886ca7c80586a58f140dd81781
to_binio <tobias.frischmann1@gmail.com>
2023-12-15 09:08:25 +0100

    day 14 part 2 performance problem

--- day14/day14.go
+++ day14/day14.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Run() {
-	lines := io.ReadLines("resources/day14/input.txt")
+	lines := io.ReadLines("resources/day14/example.txt")
 
 	var grid [][]rune
 
@@ -19,12 +19,24 @@ func Run() {
 		grid = append(grid, gridLine)
 	}
 
-	grid = moveRocksUp(grid)
+	for i := 0; i < 1000_000_000; i++ {
+		grid = moveRocksUp(grid)
+		grid = moveRocksLeft(grid)
+		grid = moveRocksDown(grid)
+		grid = moveRocksRight(grid)
+
+		if i%1000_000 == 0 {
+			println(i)
+		}
+	}
+
 	println(getWeight(grid))
 }
 
 func moveRocksUp(grid [][]rune) [][]rune {
-	for i := 0; i < len(grid); i++ {
+	hasChanges := true
+	for hasChanges {
+		hasChanges = false
 		for y := range grid {
 			for x := range grid[y] {
 				if grid[y][x] != 'O' {
@@ -37,6 +49,83 @@ func moveRocksUp(grid [][]rune) [][]rune {
 
 				grid[y][x] = '.'
 				grid[y-1][x] = 'O'
+
+				hasChanges = true
+			}
+		}
+	}
+
+	return grid
+}
+
+func moveRocksDown(grid [][]rune) [][]rune {
+	hasChanges := true
+	for hasChanges {
+		hasChanges = false
+		for y := len(grid) - 1; y >= 0; y-- {
+			for x := range grid[y] {
+				if grid[y][x] != 'O' {
+					continue
+				}
+
+				if y+1 >= len(grid) || grid[y+1][x] != '.' {
+					continue
+				}
+
+				grid[y][x] = '.'
+				grid[y+1][x] = 'O'
+
+				hasChanges = true
+			}
+		}
+	}
+
+	return grid
+}
+
+func moveRocksLeft(grid [][]rune) [][]rune {
+	hasChanges := true
+	for hasChanges {
+		hasChanges = false
+		for x := range grid[0] {
+			for y := range grid {
+				if grid[y][x] != 'O' {
+					continue
+				}
+
+				if x-1 < 0 || grid[y][x-1] != '.' {
+					continue
+				}
+
+				grid[y][x] = '.'
+				grid[y][x-1] = 'O'
+
+				hasChanges = true
+			}
+		}
+	}
+
+	return grid
+}
+
+func moveRocksRight(grid [][]rune) [][]rune {
+	hasChanges := true
+	for hasChanges {
+		hasChanges = false
+		for x := len(grid[0]) - 1; x >= 0; x-- {
+			for y := range grid {
+				if grid[y][x] != 'O' {
+					continue
+				}
+
+				if x+1 >= len(grid[0]) || grid[y][x+1] != '.' {
+					continue
+				}
+
+				grid[y][x] = '.'
+				grid[y][x+1] = 'O'
+
+				hasChanges = true
 			}
 		}
 	}
