834285d632c12e50957d88f09df558ccc71612ec
tobinio <Tobias.frischmann1@gmail.com>
2023-12-16 09:33:24 +0100

    day15 star 2 passing

--- day15/day15.go
+++ day15/day15.go
@@ -2,21 +2,40 @@ package day15
 
 import (
 	"advent-of-code-2023/io"
+	"strconv"
 	"strings"
 )
 
 func Run() {
 	line := io.ReadLines("resources/day15/input.txt")[0]
 
-	elements := strings.Split(line, ",")
+	var boxes []Box
 
-	sum := 0
-
-	for _, element := range elements {
-		sum += hash(element)
+	for i := 0; i < 256; i++ {
+		boxes = append(boxes, Box{lenses: []Lens{}})
 	}
 
-	println(sum)
+	elements := strings.Split(line, ",")
+
+	for _, element := range elements {
+		if strings.HasSuffix(element, "-") {
+			label, _ := strings.CutSuffix(element, "-")
+
+			boxIndex := hash(label)
+			boxes[boxIndex] = removeFromBox(boxes[boxIndex], label)
+		} else {
+			split := strings.Split(element, "=")
+
+			label := split[0]
+			focalLength, _ := strconv.Atoi(split[1])
+
+			boxIndex := hash(label)
+
+			boxes[boxIndex] = addLensToBox(boxes[boxIndex], Lens{label: label, focalLength: focalLength})
+		}
+	}
+
+	println(computeFocusingPower(boxes))
 }
 
 func hash(text string) int {
@@ -29,4 +48,49 @@ func hash(text string) int {
 	}
 
 	return value
+}
+
+type Lens struct {
+	label       string
+	focalLength int
+}
+
+type Box struct {
+	lenses []Lens
+}
+
+func removeFromBox(box Box, label string) Box {
+	for i, lens := range box.lenses {
+		if lens.label == label {
+			box.lenses = append(box.lenses[:i], box.lenses[i+1:]...)
+			break
+		}
+	}
+
+	return box
+}
+
+func addLensToBox(box Box, lens Lens) Box {
+	for i, otherLens := range box.lenses {
+		if otherLens.label == lens.label {
+			box.lenses[i] = lens
+			return box
+		}
+	}
+
+	box.lenses = append(box.lenses, lens)
+
+	return box
+}
+
+func computeFocusingPower(boxes []Box) int {
+	power := 0
+
+	for boxIndex, box := range boxes {
+		for lensIndex, lens := range box.lenses {
+			power += (boxIndex + 1) * (lensIndex + 1) * lens.focalLength
+		}
+	}
+
+	return power
 }
